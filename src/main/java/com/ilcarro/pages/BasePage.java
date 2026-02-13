a6de4992e01afa6b84e2b390860bd0b72a83052a
Oleh Hanziienko <ohanziienko@hotmail.com>
2026-02-13 03:00:23 +0100

    Initial commit

---
+++ src/main/java/com/ilcarro/pages/BasePage.java
@@ -0,0 +1,66 @@
+package com.ilcarro.pages;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BasePage {
+
+    public static WebDriver driver;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    public void launchBrowser() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    public void openUrl() {
+        driver.get("https://ilcarro.web.app");
+    }
+
+    public void click(WebElement element) {
+        element.click();
+    }
+
+    public void type(WebElement element, String text) {
+        if (text != null) {
+            click(element);
+            element.clear();
+            element.sendKeys(text);
+        }
+    }
+
+    public WebDriverWait getWait(int seconds) {
+        return new WebDriverWait(driver, Duration.ofSeconds(seconds));
+    }
+
+    public boolean shouldHaveText(WebElement element, String text, int time) {
+        return getWait(time)
+                .until(ExpectedConditions.textToBePresentInElement(element,text));
+    }
+
+    public boolean isElementVisible(WebElement element) {
+        try {
+            element.isDisplayed();
+            return true;
+        } catch (NoAlertPresentException e) {
+            e.getMessage();
+            return false;
+        }
+    }
+}
+
+
---
+++ src/test/resources/features/navigate.feature
@@ -0,0 +1,6 @@
+Feature: HomePage
+
+  Scenario: Verify HomePage title is displayed
+    Given User launches Chrome browser
+    When User opens ilcarro HomePage
+    Then User verifies HomePage title is displayed
